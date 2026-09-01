63de65521ada3e898d3e9b1918a1d53fdefce95b
Legacy-6-7 <reservedplanet1233@gmail.com>
2026-09-01 22:23:22 +0530

    Update homepage and navbar

--- onlineadmissionsystem/settings.py
+++ onlineadmissionsystem/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 import os
 
-SECRET_KEY = os.environ.get("fb!5nuk7x94a4)xttn@yd_q3#n-_2ob=fim-alpimk2_dls#^_")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
